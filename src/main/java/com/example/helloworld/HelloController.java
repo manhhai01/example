d997142eaae35cb537f9c781a1994c4a8afd40b9
manhhai01 <lahai7744@gmail.com>
2026-01-27 23:28:21 +0700

    fix/jenkins

--- src/main/java/com/example/helloworld/HelloController.java
+++ src/main/java/com/example/helloworld/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World from Jenkins!";
+        return "Hello World from Jenkins 2!";
     }
 }
 
